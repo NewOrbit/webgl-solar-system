002ccfccacaa3641fd94fd2959f874254fe8451e
James Monger <jkmonger96@gmail.com>
2017-12-14 17:05:58 +0000

    Use solid colour for moon

--- src/objects/moon.ts
+++ src/objects/moon.ts
@@ -1,11 +1,9 @@
 import * as THREE from 'three';
 
 export default function createMoon() {
-    const texture = new THREE.TextureLoader().load("./public/textures/moon.png");
-
     const geometry = new THREE.SphereGeometry(2, 50, 50);
     const material = new THREE.MeshPhongMaterial({
-        map: texture,
+        color: 0x999999,
         flatShading: true,
         shininess: 0
     });
